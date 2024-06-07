823cd0b03a506b2026e7efe67c8eac5f649f783a
Sithila-Sihan-Somaratne <chutimalli182@gmail.com>
2024-06-07 20:37:39 +0200

    Moved away UOM...

--- ODD_or_EVEN.py
+++
@@ -1,8 +0,0 @@
-num = input("Input a number : ")
-number = int(num)
-odd_or_even = number % 2
-if odd_or_even == 0:
-    print(f"{num} is an even number.")
-else:
-    print(f"{num} is an odd number.")
-
